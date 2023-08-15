23c3b055218cdd6e8d4f349228e28b1eeae40b66
mgr_inz_rafal <rchabowski@gmail.com>
2023-08-15 17:10:50 +0200

    Move blobs sideways

--- src/engine.rs
+++ src/engine.rs
@@ -1,3 +1,7 @@
+use std::thread::Thread;
+
+use rand::{rngs::ThreadRng, Rng};
+
 use crate::{
     blob_detector::BlobDetector,
     blobs::Blobs,
@@ -9,11 +13,16 @@ use crate::{
 pub(crate) struct Engine {
     board: Board,
     blobs: Blobs,
+    rng: ThreadRng,
 }
 
 impl Engine {
     pub(crate) fn new(board: Board, blobs: Blobs) -> Self {
-        Self { board, blobs }
+        Self {
+            board,
+            blobs,
+            rng: rand::thread_rng(),
+        }
     }
 
     fn board(&self) -> &Board {
@@ -25,13 +34,43 @@ impl Engine {
     }
 
     pub(crate) fn tick(self) -> Engine {
-        let Engine { mut board, blobs } = self;
+        let Engine {
+            mut board,
+            blobs,
+            mut rng,
+        } = self;
 
         for (_, points) in blobs {
             for pt in points.iter().rev() {
+                // Try move down
                 let dest_pt = Point::new(pt.x(), pt.y() + 1);
                 if board.tiles().at(dest_pt.x(), dest_pt.y()).is_air() {
                     board.swap(pt.x(), pt.y(), pt.x(), pt.y() + 1);
+                } else {
+                    // Didn't move down, try sideways
+                    let dest_pt_left = Point::new(pt.x() - 1, pt.y());
+                    let dest_pt_right = Point::new(pt.x() + 1, pt.y());
+                    match (
+                        board
+                            .tiles()
+                            .at(dest_pt_left.x(), dest_pt_left.y())
+                            .is_air(),
+                        board
+                            .tiles()
+                            .at(dest_pt_right.x(), dest_pt_right.y())
+                            .is_air(),
+                    ) {
+                        (true, true) => {
+                            if rng.gen::<bool>() {
+                                board.swap(pt.x(), pt.y(), pt.x() - 1, pt.y())
+                            } else {
+                                board.swap(pt.x(), pt.y(), pt.x() + 1, pt.y())
+                            }
+                        }
+                        (true, false) => board.swap(pt.x(), pt.y(), pt.x() - 1, pt.y()),
+                        (false, true) => board.swap(pt.x(), pt.y(), pt.x() + 1, pt.y()),
+                        (false, false) => (),
+                    }
                 }
             }
         }
@@ -41,7 +80,7 @@ impl Engine {
         let blob_detector = BlobDetector::new(&board);
         let blobs = blob_detector.detect();
 
-        Engine { board, blobs }
+        Engine { board, blobs, rng }
     }
 }
 
